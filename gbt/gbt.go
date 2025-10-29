9602ae4b521f600543118fc3c638d9da5a2af66c
D. Bohdan <dbohdan@dbohdan.com>
2025-10-29 17:42:33 +0000

    feat(gbt): improve precision like gbtgui

--- gbt/gbt.go
+++ gbt/gbt.go
@@ -9,14 +9,15 @@ import (
 func main() {
 	g := tai.Now().AsGregorian()
 
-	// pull the hour, minute, and second from the TAI time and convert the
-	// hour and minute to seconds
-	hourSeconds := g.Hour * 3600
-	minuteSeconds := g.Min * 60
-	seconds := g.Sec
+	// pull the hour, minute, second, and attosecond from the TAI time
+	// and convert them to milliseconds
+	h := g.Hour * 3_600_000
+	m := g.Min * 60_000
+	s := g.Sec * 1_000
+	ms := g.Asec / 1_000_000_000_000_000
 
 	// do the math for beatTAI and store it
-	beatTAI := float64(hourSeconds+minuteSeconds+seconds) / 86.4
+	beatTAI := (float64(h+m+s) + float64(ms)) / 86400
 
 	// print the value, rounded to two decimal places, padded with leading zeroes
 	// if necessary and prefixed with : for proper beatTAI syntax
