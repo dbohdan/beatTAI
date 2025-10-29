861470b84c994de4b235cd67ef543be20e7991a4
D. Bohdan <dbohdan@dbohdan.com>
2025-10-29 10:57:52 +0000

    fix(i9w): fix accidental UTC output
    The way libtai is currently used generates beats in UTC
    that include the leap seconds.
    We need to hack around the normal libtai API a bit
    to obtain the raw fractional seconds we can convert to beatTAI.
    
    This fix matches the output of the Go implementation.
    I have checked it, and it's 37 seconds ahead of UTC,
    which is correct for TAI since 2017.
    
    Update the prefix from "@" to ":".

--- i9w/i9w.c
+++ i9w/i9w.c
@@ -1,21 +1,39 @@
+#include <math.h>
 #include <stdio.h>
+
 #include "vendor/libtai/caltime.h"
 #include "vendor/libtai/leapsecs.h"
-#include "vendor/libtai/tai.h"
-
-struct tai sec;
-struct caltime ct;
+#include "vendor/libtai/taia.h"
 
 int main(int argc,char *argv[]) {
+  struct caltime ref_ct;
+  struct taia ref;
+  struct taia t;
+
   if (leapsecs_init() == -1) {
-    fprintf(stderr,"fatal: unable to init leapsecs\n");
+    fprintf(stderr, "fatal: unable to init leapsecs\n");
     return 111;
   }
-  tai_now(&sec);
-  caltime_utc(&ct,&sec,(int *) 0,(int *) 0);
-  unsigned int milibeats=(((((ct.hour) * 3600) + (ct.minute * 60) + ct.second)) * 10000) / 864;
-  unsigned int wholebeats=milibeats / 1000;
-  unsigned int partialbeats=milibeats % 1000;
-  printf("@%u.%02u\n",wholebeats,partialbeats/10);
+
+  if (!caltime_scan("1970-01-01 00:00:00 +0000", &ref_ct)) {
+    fprintf(stderr, "fatal: failed to scan reference time\n");
+    return 111;
+  }
+
+  // We need to zero the nanosecond and attosecond field of the reference ourselves.
+  taia_sub(&ref, &ref, &ref);
+  caltime_tai(&ref_ct, &ref.sec);
+
+  taia_now(&t);
+  leapsecs_add(&t.sec, 1);
+  // Both tai_now and caltime_scan add a constant offset to seconds.
+  // Subtracting the reference removes the offset, leaving elapsed seconds.
+  taia_sub(&t, &t, &ref);
+
+  // leapsecs_add does not include the 10 leap seconds pre-added to TAI.
+  double millisec = (taia_approx(&t) + 10) * 1000;
+  double beats = fmod(millisec / 86400, 1000);
+  printf(":%06.2f\n", beats);
+
   return 0;
 }
